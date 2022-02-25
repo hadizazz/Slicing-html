0b375f93b0ba68572a39e2a2d7e965e1d84377fb
hadizazz <hadizazz81@gmail.com>
2022-02-25 17:05:17 +0700

    commit on master

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { AiOutlineArrowRight } from "react-icons/ai";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
+import kolektive from "../images/kolektive.png";
+import { FaTag } from "react-icons/fa";
+import image from "../images/personal.jpg"
+
 export default function landingpage() {
   return (
     <div className="body">
@@ -30,12 +34,15 @@ export default function landingpage() {
         <h1 class="title">Project</h1>
         <div class="content">
           <div class="box ">
-            <h2>lorem</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-              iure dolore deserunt repudiandae nostrum repellat, quos tempore
-              assumenda, eos voluptatibus alias numquam, sit
-            </p>
+            <img src={kolektive} className="imgProject" />
+            <h1>Kolektive</h1>
+            <div className="Tag">
+              <FaTag />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Pariatur iure dolore deserunt repudiandae nostrum repellat,
+              </p>
+            </div>
             <button>
               See Project{" "}
               <span>
@@ -44,12 +51,15 @@ export default function landingpage() {
             </button>
           </div>
           <div class="box ">
-            <h2>Lorem</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-              iure dolore deserunt repudiandae nostrum repellat, quos tempore
-              assumenda, eos voluptatibus alias numquam, sit
-            </p>
+            <img src={image} className="imgProject" />
+            <h1>Lorem</h1>
+            <div className="Tag">
+              <FaTag />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Pariatur iure dolore deserunt repudiandae nostrum repellat,
+              </p>
+            </div>
             <button>
               See Project{" "}
               <span>
@@ -57,13 +67,16 @@ export default function landingpage() {
               </span>
             </button>
           </div>
-          <div class="box">
-            <h2>Lorem</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-              iure dolore deserunt repudiandae nostrum repellat, quos tempore
-              assumenda, eos voluptatibus alias numquam, sit
-            </p>
+          <div class="box ">
+            <img src={image} className="imgProject" />
+            <h1>Lorem</h1>
+            <div className="Tag">
+              <FaTag />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Pariatur iure dolore deserunt repudiandae nostrum repellat,
+              </p>
+            </div>
             <button>
               See Project{" "}
               <span>
@@ -72,22 +85,67 @@ export default function landingpage() {
             </button>
           </div>
         </div>
+
+        {/* <div>
+          <div className="flex item-center">
+            <h3 class="font-semibold text-gray-700 mt-5"> Project </h3>
+          </div>
+
+          <div class="mt-10 grid grid-rows-auto grid-cols-1 md:grid-rows2 md:grid-cols-2 lg:grid-rows-1 lg:grid-cols-4 gap-10">
+            <div>
+              <div class="w-auto rounded-3x1 shadow">
+                <img
+                  src="../images/kolektive.png"
+                  class="object-cover rounded-t-3x1 w-full h-auto"
+                />
+                <div class="p-4 h-56 md:h-52 lg:h-64 flex flex-col justify-between">
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <h5 class="font-bold">Kolektive</h5>
+                    </div>
+                    <div className="flex mt-2">
+                      <p class="ml-3 text-gray-700">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Pariatur iure dolore deserunt repudiandae nostrum
+                        repellat, quos tempore assumenda, eos voluptatibus alias
+                        numquam, sit
+                      </p>
+                    </div>
+                  </div>
+                  <a
+                    target="_blank"
+                    href="https://kolektive.netlify.app/"
+                    class="flex justify-between items-center bg-blue-500 hover:bg-blue-400 px-7 py-2 rounded-full"
+                  >
+                    <h6 class="text-white text-center">See Project</h6>
+                    <AiOutlineArrowRight />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </section>
       <section id="contact">
         <h1>Contact Us</h1>
         <div className="boxForm">
           <form id="form">
             <div className="forms">
-              <label for="name" >Name </label>
-              <input type="text" id="name" className="input"/>
+              <label for="name">Name </label>
+              <input type="text" id="name" className="input" />
             </div>
             <div className="forms">
               <label for="email">Email</label>
-              <input type="email" id="email" className="input"/>
+              <input type="email" id="email" className="input" />
             </div>
             <div className="forms">
               <label for="Message">Message </label>
-              <textarea id="message" className="input" name="message" style={{height:"200px"}}/>
+              <textarea
+                id="message"
+                className="input"
+                name="message"
+                style={{ height: "200px" }}
+              />
             </div>
             <input type="submit" value="submit"></input>
             {/* <button type="submit" >
@@ -96,7 +154,7 @@ export default function landingpage() {
           </form>
         </div>
       </section>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
